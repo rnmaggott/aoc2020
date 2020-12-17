98145345424f47e2633011d7c6e8ccc1e48b22d3
Ryan Maggott <rnmaggott@gmail.com>
2020-12-17 13:57:56 +0200

    Day4 Part2 Done.

--- day4/day4.py
+++ day4/day4.py
@@ -1,4 +1,5 @@
 # %%
+import string
 with open("/mnt/c/Users/Ryan/Documents/aoc2020/day4/input.txt") as f:
     lines = f.read().split("\n\n")
 
@@ -7,6 +8,50 @@ with open("/mnt/c/Users/Ryan/Documents/aoc2020/day4/input.txt") as f:
 def check_codes(entry, expected_codes):
     for batch in entry.replace("\n", " ").split(" "):
         code = batch.split(":")[0]
+        value = batch.split(":")[1]
+
+        if code == "byr":
+            if (int(value) < 1920) or (int(value) > 2002):
+                return False
+        if code == "iyr":
+            if (int(value) < 2010) or (int(value) > 2020):
+                return False
+        if code == "eyr":
+            if (int(value) < 2020) or (int(value) > 2030):
+                return False
+        if code == "hgt":
+            unit = value[-2:]
+            value = value[:-2]
+            if unit == "cm":
+                if (int(value) < 150) or (int(value) > 193):
+                    return False
+            elif unit == "in":
+                if (int(value) < 59) or (int(value) > 76):
+                    return False
+            else:
+                return False
+        if code == "hcl":
+            try:
+                if value[0] != "#":
+                    return False
+                value = value[1:]
+                if len(value) != 6:
+                    return False
+                if not all(c in string.hexdigits for c in value):
+                    return False
+            except ValueError:
+                return False
+        if code == "ecl":
+            if value not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+                return False
+        if code == "pid":
+            if len(value) != 9:
+                return False
+            try:
+                int(value)
+            except ValueError:
+                return False
+
         try:
             expected_codes.pop(expected_codes.index(code))
         except ValueError:
@@ -34,7 +79,7 @@ expected_codes = ["byr",
 
 valid_passports = 0
 
-for entry in lines:
+for pos, entry in enumerate(lines):
     if check_codes(entry, expected_codes.copy()):
         valid_passports += 1
 
