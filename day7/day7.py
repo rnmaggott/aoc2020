07aef12fde97899257a28e368d27ad131ae310dc
Ryan Maggott <rnmaggott@gmail.com>
2020-12-18 16:53:40 +0200

    Day7 Part2 Done.

--- day7/day7.py
+++ day7/day7.py
@@ -3,6 +3,9 @@ import re
 with open("/mnt/c/Users/Ryan/Documents/aoc2020/day7/input.txt") as f:
     lines = f.read().split("\n")
 
+with open("/mnt/c/Users/Ryan/Documents/aoc2020/day7/input.txt") as f:
+    nested_bag_input = f.read().split("\n")
+
 
 # %%
 def bagCheck(bag, bag_dict, bag_list=[]):
@@ -46,10 +49,37 @@ def createBagDict(lines):
 
 
 # %%
+def countBagsInChildren(parent, bag_dict):
+    print(f"Counting bags in parent: {parent}")
+    if bag_dict[parent] is None:
+        print(f"Parent {parent} has no more children")
+        return 0
+    bags = 0
+    print("Looking through children")
+    for child in bag_dict[parent]:
+        child_bags = countBagsInChildren(child, bag_dict)
+        print(
+            f"Child {child} has {child_bags}, " +
+            f"adding {bag_dict[parent][child]*child_bags} to total")
+        bags += bag_dict[parent][child]*child_bags + bag_dict[parent][child]
+
+    return bags
+
+
+# %%
 bag_dict = createBagDict(lines)
 input_criteria = "shiny gold bag"
 input_criteria = input_criteria.rsplit(" ", 1)[0]
 output = bagCheck(input_criteria, bag_dict)
 
-print(len(output))
+print(
+    f"There are {len(output)} color options to contain your {input_criteria} "
+    "bag.")
+# %%
+# input_criteria = "dark green bag"
+# input_criteria = input_criteria.rsplit(" ", 1)[0]
+nested_bag_dict = createBagDict(nested_bag_input)
+# print(findNestedBags(input_criteria, nested_bag_dict))
+print(countBagsInChildren(input_criteria, nested_bag_dict))
+
 # %%
